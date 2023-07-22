a57954d90d39ed4aa176246bf8c3b94e6a4305f3
Euan Cunningham <euancc06@gmail.com>
2023-07-22 17:00:11 +1200

    add server folder

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default {
-  build: {
-    outDir: 'dist',
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
   },
-}
+})
