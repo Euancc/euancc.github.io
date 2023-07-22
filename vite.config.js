9e9b7891412f61cab614608b5fea92e7b6258080
Euan Cunningham <euancc06@gmail.com>
2023-07-22 16:51:37 +1200

    configure vite file again

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/', // Replace '/subpath/' with your actual subpath
-  plugins: [react()],
-  // other configuration options...
-})
+export default {
+  build: {
+    outDir: 'dist',
+  },
+}
